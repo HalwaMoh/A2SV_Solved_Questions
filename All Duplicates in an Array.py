8a85875d59afaa10cab3758e66bdbdb88c30c48b
HalwaMoh <halwam49@gmail.com>
2026-02-06 11:16:23 +0300

    Implement findDuplicates method to identify duplicates

---
+++ All Duplicates in an Array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        
+        res = []
+
+        for num in nums:
+            idx = abs(num) - 1  
+            
+            
+            if nums[idx] < 0:
+                res.append(abs(num))
+            else:
+               
+                nums[idx] = -nums[idx]
+
+        return res
