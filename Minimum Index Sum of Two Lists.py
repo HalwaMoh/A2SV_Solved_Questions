c42af74a1f8b57ab8a111869bef068db6fccb695
HalwaMoh <halwam49@gmail.com>
2026-02-06 10:42:49 +0300

    Implement solution for minimum index sum of two lists

---
+++ Minimum Index Sum of Two Lists.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        index_map = {s: i for i, s in enumerate(list1)}
+        
+        min_sum = float('inf')
+        result = []
+
+        for j, s in enumerate(list2):
+            if s in index_map:
+                idx_sum = j + index_map[s]
+                
+                if idx_sum < min_sum:
+                    min_sum = idx_sum
+                    result = [s]  
+                elif idx_sum == min_sum:
+                    result.append(s)  
+        return result
