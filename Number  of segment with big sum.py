6d96b0bd9af323f0ae1f388feb7b2d4652817dca
HalwaMoh <halwam49@gmail.com>
2026-02-27 21:28:03 +0300

    Create Number  of segment with big sum.py

---
+++ Number  of segment with big sum.py
@@ -0,0 +1,19 @@
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+
+left = 0
+curr_sum = 0
+min_len = float('inf')
+
+for right in range(n):
+    curr_sum += a[right]
+
+    while curr_sum >= s:
+        min_len = min(min_len, right - left + 1)
+        curr_sum -= a[left]
+        left += 1
+
+if min_len == float('inf'):
+    print(-1)
+else:
+    print(min_len)
