063c1be495897ea147297db1cb80bc08809c3de8
HalwaMoh <halwam49@gmail.com>
2026-03-06 10:52:13 +0300

    Create Binary Subarrays With Sum.py

---
+++ Binary Subarrays With Sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+
+       
+        prefix_sum = 0
+        hashmap = {0:1}
+        count = 0
+
+        for num in nums:
+            prefix_sum += num
+            
+            if prefix_sum - goal in hashmap:
+                count += hashmap[prefix_sum - goal]
+
+            hashmap[prefix_sum] = hashmap.get(prefix_sum, 0) + 1
+
+        return count
