4262ff5a4a3990d7a9c41ede715cfe62aeb7fed0
HalwaMoh <halwam49@gmail.com>
2026-03-22 01:58:09 +0300

    Add solution for Minimum Moves to Reach Target Score

---
+++ leetcode/2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minMoves(self, target, maxDoubles):
+        count = 0
+        
+        while target > 1 and maxDoubles > 0:
+            if target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+            else:
+                target -= 1
+            count += 1
+        
+        return count + (target - 1)
