052416fd4894552f02883e083444561a2e9ce96a
HalwaMoh <halwam49@gmail.com>
2026-04-17 21:32:20 +0300

    Time: 9351 ms (5.39%), Space: 33.3 MB (90%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -1,25 +1,6 @@
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
-        def mergesort(arr):
-            if len(arr) <=1:
-                return arr
-            mid=len(arr)//2
-            left=mergesort(arr[ : mid])
-            right= mergesort( arr[mid: ])
-            
-            l,r=0,0
-            merged=[]
-            while l<=len(left) and r <= len(right):
-                if left[l] < right[r]:
-                    merged.append(left[l])
-                    l +=1
-                else:
-                    merged.append(right[r])
-                    r +=1  
-            merged.extend(left[l:])
-            merged.extend(right[r:] )
-
-            return merged
+        
         mod=10 **9 +7
         from bisect import bisect_left,bisect_right
         arr=[]
