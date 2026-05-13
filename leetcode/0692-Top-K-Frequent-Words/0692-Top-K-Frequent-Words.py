dc6a6731071a6585ab48eac6ad3b460c318acd81
HalwaMoh <halwam49@gmail.com>
2026-05-13 17:21:24 +0300

    Add solution for Top K Frequent Words

---
+++ leetcode/0692-Top-K-Frequent-Words/0692-Top-K-Frequent-Words.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        wordmap=Counter(words)
+        minheap=[(-freq ,word) for word , freq in wordmap.items()]
+        heapq.heapify(minheap)
+        res=[]
+        while k >0:
+            freq , word = heapq.heappop(minheap)
+            res.append(word)
+            k -=1
+        return res
